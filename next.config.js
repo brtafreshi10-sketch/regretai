94197ee27f3f7bb1a6d44142984ce11b5fcdf57a
Brooks Tafreshi <brtafreshi2023@gmail.com>
2026-06-19 11:44:55 -0500

    site update

--- next.config.js
+++ next.config.js
@@ -1,19 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',       // Tells Next.js to create a static 'out' folder for iOS
-  trailingSlash: true,    // Required for static routing inside the iOS app view
+  trailingSlash: true,
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
   eslint: {
-    // This allows the production build to successfully complete 
-    // even if your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // This allows the production build to successfully complete
-    // even if your project has TypeScript type errors.
     ignoreBuildErrors: true,
   },
 };
